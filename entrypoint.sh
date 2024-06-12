9246466193df525803f5feebb0fca963d8876f58
Oleh Smolynets <smolynets@gmail.com>
2024-06-12 18:20:33 +0300

    add docker-compose

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if Alembic is installed
+alembic --version
+
+# Upgrade the database
+alembic upgrade head
+
+echo "#############################sdfsfddzfsfdsdzf###########"
+
+# Start the FastAPI application
+uvicorn backend.main:app --host 0.0.0.0 --port 8000
